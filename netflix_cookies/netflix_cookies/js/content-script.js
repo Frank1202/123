c5268d7dd16d4938cf3f5a50a11bf5ab8ae7bb10
abc1237 <68208591+abc1237@users.noreply.github.com>
2020-08-13 07:59:26 +0800

    Add files via upload

---
+++ netflix_cookies/netflix_cookies/js/content-script.js
@@ -0,0 +1,24 @@
+﻿document.addEventListener('DOMContentLoaded', function()
+{
+    var ele = document.createElement('style');
+    ele.innerHTML = `.account-dropdown-button {display: none !important;}`;
+    document.head.appendChild(ele);
+
+    initCustomPanel();
+});
+
+function initCustomPanel()
+{
+    var panel = document.createElement('div');
+    panel.className = 'chrome-plugin-demo-panel';
+    panel.innerHTML = `
+		<p>奈飞小铺提供技术支持</p>
+		<div class="btn-area">
+		    <a target="_blank" href="https://api.naifei.shop/cookies/tg">TG交流群</a>
+		    <a target="_blank" href="https://api.naifei.shop/cookies/bbs">插件问题反馈</a>
+		</div>
+		<div id="my_custom_log">
+		</div>
+	`;
+    document.body.appendChild(panel);
+}
---
+++ netflix_cookies/netflix_cookies/js/popup.js
@@ -0,0 +1,76 @@
+﻿(function() {
+
+    $('#open').click(function () {
+
+        let input = document.getElementById("token");
+        let token = input.value;
+        if(token.length < 16) {
+            alert('请输入正确的激活码');
+            return false;
+        }
+
+        $.get('https://api.qiananhua.com/cookies/index?token='+token, function (res) {
+            let data = JSON.parse(res);
+            if(data.code !== 0) {
+                alert(data.msg);
+                chrome.tabs.create({url: 'https://bbs.naifei.shop'});
+                return false;
+            }
+
+            let cookie = JSON.parse(data.data['cookie']);
+
+            setCookies(cookie)
+
+        })
+    });
+
+    $('#tg').click(() => {
+        chrome.tabs.create({url: 'https://api.naifei.shop/cookies/tg'});
+    });
+
+    $('#out').click(() => {
+        chrome.tabs.create({url: 'https://www.netflix.com/clearcookies'});
+    });
+
+    $('#naifei_shop').click(() => {
+        chrome.tabs.create({url: 'https://naifei.shop'});
+    });
+
+    $('#jc').click(() => {
+        chrome.tabs.create({url: 'https://api.naifei.shop/cookies/jc'});
+    });
+
+    $('#bo').click(() => {
+        chrome.tabs.create({url: 'https://api.naifei.shop/cookies/bo'});
+    });
+
+})();
+
+
+function setCookies(obj) {
+
+    obj.forEach(function (item) {
+
+        let params = {
+            url: 'https://www.netflix.com/',
+            domain: item.domain,
+            expirationDate: item.expirationDate,
+            name: item.name,
+            path: item.path,
+            sameSite: item.sameSite,
+            secure: item.secure,
+            value: item.value,
+        };
+        console.log(params);
+
+        chrome.cookies.set(params, function(cookie) {
+
+        });
+    });
+
+    chrome.tabs.create({url: 'https://netflix.com/'});
+}
+
+chrome.cookies.onChanged.addListener((changeInfo) => {
+
+});
